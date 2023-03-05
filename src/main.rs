fac91a0fbaea8701b6c0d6908b00668665ba7a2a
Vitor Trimer <vitortrimer@gmail.com>
2023-03-05 17:21:07 -0300

    migrade from termion to ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use termion::{color, style, terminal_size};
-use std::io;
+use ncurses::*;
 
 fn main() {
-    println!("{}{}Stuff", termion::clear::All, termion::cursor::Goto(1, 1));
-    println!("{:?} terminal_size", terminal_size());
-    println!("{}Red", color::Fg(color::Red));
-    println!("{}Blue", color::Fg(color::Blue));
-    println!("{}Plain Italic", style::Italic);
+    initscr();
+
+    addstr("Hello, world!");
+
+    refresh();
+    getch();
+
+    endwin();
 }
